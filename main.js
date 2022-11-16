cef0109b108e48643d18c609089bec90b406f8a3
vivia.amorim <vivian.amorim@squadra.com.br>
2022-11-16 17:16:23 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+function createGame(player1, hour, player2) {
+  return `
+                <li>
+                <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+                <strong>${hour}</strong>
+                <img src="/assets/icon-${player2}.svg" alt="Bandeira do ${player2}" />
+              </li>
+  `
+}
+
+function createCard(date, day, games) {
+  return `
+          <div class="card">
+            <h2>${date} <span>${day}</span></h2>
+            <ul>
+            ${games}
+            </ul>
+          </div>
+  `
+}
+
+document.querySelector("#app").innerHTML = `
+    <header>
+        <img src="./assets/logo.svg" alt="Logo da NLW" />
+    </header>
+      <main id="card">
+        ${createCard(
+          "24/11",
+          "quinta",
+          createGame("brazil", "16:00", "serbia")
+        )}
+        ${createCard(
+          "28/11",
+          "segunda",
+          createGame("brazil", "13:00", "suica") +
+            createGame("portugal", "16:00", "uruguai")
+        )}
+        ${createCard(
+          "02/12",
+          "sexta",
+          createGame("brazil", "16:00", "camaroes")
+        )}
+      </main>
+`
